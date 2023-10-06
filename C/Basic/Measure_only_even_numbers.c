b3d1424d929ca5295c255d9e1d86d7f95e4d975c
Ornan <ornanmatos@outlook.com>
2023-10-05 23:11:08 -0300

    feat: Criar aplicação simples para calcular media, somente números pares inteiros.

---
+++ C/Basic/Measure_only_even_numbers.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/*
+O programa recebe números do usuário e calcula a media dos números digitados, somente números pares. A leitura irá ser encerrada se o usuário digitar zero.
+*/
+
+int main(){
+
+    int num; // numero digitado pelo usuário
+    int soma = 0; // soma dos número digitados pelo usuário
+
+    printf("\n\nDigite números pares para obter a media (pressione 0 para sair)\n\n)"); // mensagem inicial
+
+    do
+    {
+        printf("\n\nDigite um número:"); // mensagem para o usuário
+        scanf("%d", &num);  // leitura do numero digitado pelo usuário
+
+        soma = soma + num; // soma dos números digitados pelo usuário
+
+    } while (num != 0); // condição de parada do loop
+    
+    
+    if (soma %2 == 0) // condição para verificar se a soma é par ou impar
+    {
+        int media = soma / 2; // calculo da media
+        printf("\n\nMedia: %d\n", media); // impressão da media
+    }
+
+    else
+    {
+        printf("\n\n Desculpe! A somas dos valores inseridos não é par.\n"); 
+    }
+    
+    return 0; 
+    
+}
