175f64945d837e12019f31faaeee6b62fa8ae3af
Ornan <ornanmatos@outlook.com>
2023-10-08 23:43:46 -0300

    docs: Adicionar comentários as funções variáveis.

--- C/Basic/Area_calculation_circle.c
+++ C/Basic/Area_calculation_circle.c
@@ -6,19 +6,19 @@
 O programa irá calcular a área de um círculo de raio r, testando ao final se o valor da area é menor que zero.
 */
 
-int main()
+int main() 
 {
-    float diametro;
-    printf("\nInsira o diâmetro do circulo: ");
-    scanf("%f", &diametro);
+    float diametro; // variavel para receber o valor do diametro
+    printf("\nInsira o diâmetro do circulo: "); // solicita o valor do diametro
+    scanf("%f", &diametro); // recebe o valor do diametro
     printf("\n\n");
     float raio = diametro / 2;
-    float area = pow(raio, 2) * 3.1415926535898;
-    printf("Area: %.2f²\n\n", area);
+    float area = pow(raio, 2) * 3.1415926535898; // calcula a area
+    printf("Area: %.2f²\n\n", area); // imprime a area
     
     if(area < 0){
         printf("O valor é menor que zero!\n\n");
-    }
+    } // testa se a area é menor que zero
     else{
         printf("O valor é maior que zero!\n\n");
     }
