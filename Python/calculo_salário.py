90613ac9d9f7c2f033e70e4814ff786b3e767c3a
Ornan <ornanmatos@outlook.com>
2024-01-16 15:56:24 -0300

    feat:Calcular aumento de salário.

---
+++ Python/calculo_salário.py
@@ -0,0 +1,7 @@
+#calculo de aumento de salário
+salario = 750
+aumento = 5
+print("Valor do salário = R$", salario)
+print("Aumento de ", aumento, "%")
+print("Total do aumento de R$ ", salario * aumento / 100)
+print("Total R$ ", salario + (salario * aumento / 100))
