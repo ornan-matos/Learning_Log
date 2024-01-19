b9c6046415aac3a5e9610a8d3621fc2aad7c11f0
Ornan <ornanmatos@outlook.com>
2024-01-18 23:58:24 -0300

    feat:Calcular a media geral de algumas variáeis.

---
+++ Python/nota_aluno.py
@@ -0,0 +1,12 @@
+#A expressão será utilizada para decidir se um aluno foi ou não aprovado.
+#Para ser aprovado a media geral precisa ser maior que 7.
+matematica = 7 
+biologia = 8
+portugues = 10
+fisica = 4
+
+media = (matematica + biologia + portugues + fisica) //4 
+
+print ("Resultado Final: ", media)
+print ("Aluno Aprovado: ", media >= 7)
+
