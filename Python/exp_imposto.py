7a1a5a90be0f1dbba23d75299225e3b1a48a82f6
Ornan <ornanmatos@outlook.com>
2024-01-19 23:35:15 -0300

    feat:Calcular elegibilidade de imposto usando operadores relacionais.

---
+++ Python/exp_imposto.py
@@ -0,0 +1,10 @@
+#A expressão determinara se uma pessoa deve ou não pagar imposto. A pessoas que pagam imposto tem o salário ammaior que R$ 1.200.
+
+p1 = 2000
+p2 = 3_000
+p3 = 900
+regra = 1_200
+print("Quem deve pagar o imposto:")
+print("Pessoa 1:", p1 >= regra)
+print("Pessoa 2:", p2 >= regra)
+print("Pessoa 3:", p3 >= regra)
