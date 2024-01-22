a5646343c3842bae543adeba83a53dd607ce7242
Ornan <ornanmatos@outlook.com>
2024-01-22 11:44:06 -0300

    feat:Compor dados de variáveis usando format.

---
+++ Python/composição.py
@@ -0,0 +1,17 @@
+nome = "Ornan Matos"
+idade = 28
+grana = 42.500
+#Composição de variáveis com format
+print(f"{nome:} tem {idade:2} anos e R${grana:2.2f} em sua carteira.")
+
+#Fatiando e Concatenando Strings
+fatia_1 = "Coca"
+fatia_2 = "Cola"
+fatia_3 = "Coca - Cola"
+print(fatia_1[0:4], "-", fatia_2[0:4])
+print(fatia_3[0:4], "-",fatia_3[7:12])
+
+
+
+
+
