7b30bfeb028d40fba397d39b3b3d72fd0ad65a2c
Ornan <ornanmatos@outlook.com>
2024-01-24 20:55:02 -0300

    feat:Receber input simples do usuário.

---
+++ Python/input.py
@@ -0,0 +1,10 @@
+nome = input("Insira o seu nome: ")
+print(f"Olá {nome}!")
+
+idade = int(input("Insira sua idade: "))
+
+salario = float(input("Insira o seu sálario: "))
+
+print(f"{nome}, você tem {idade} e ganha mensalmente R${salario:3.3f}")
+
+
