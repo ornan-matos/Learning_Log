8ce82a98be4306b424de5fa4d1984442cc95a67d
Ornan <ornanmatos@outlook.com>
2024-02-02 21:37:52 -0300

    feat:Converter temperatura de celsius para fahrenheit.

---
+++ Python/conversao_temperatura.py
@@ -0,0 +1,9 @@
+#Converter Celcius em Fahrenheit
+
+print("\n----- Conversor de Temperatura °C em °F ------\n")
+
+celsius = int(input("Temperatua em Celsius: "))
+
+fahrenheit = (celsius * (9/5)) + 32
+
+print(f"\n{fahrenheit}°F")
