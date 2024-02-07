649fc9ff249eb2f3f0c16a90d055be4e3181861e
Ornan <ornanmatos@outlook.com>
2024-02-06 22:20:41 -0300

    feat:Escolher o maior número usando estrutura de decisão simple.

---
+++ Python/maior_num_if.py
@@ -0,0 +1,12 @@
+a = int(input("Insira o primeiro valor: "))
+
+b = int(input("Insira o segundo valor: "))
+
+if a > b:
+    print("O primeiro é maior que o segundo.")
+
+if a < b:
+    print("O segundo é maior que o primeiro.")
+
+if a == b:
+    print("São iguais!")
