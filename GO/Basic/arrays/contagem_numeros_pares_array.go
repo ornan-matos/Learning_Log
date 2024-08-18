634e025f29f28053a18a5ac2d773e20c7f9ec907
Ornan <ornanmatos@outlook.com>
2024-08-17 22:43:38 -0300

    update:adicionar cabeçalho e comentários no código

--- GO/Basic/arrays/contagem_numeros_pares_array.go
+++ GO/Basic/arrays/contagem_numeros_pares_array.go
@@ -1,15 +1,25 @@
+// O programa a seguir conta e imprime o número de elementos pares em um array de inteiros de tamanho 10.
+// O array é inicializado com valores de 1 a 10. O código percorre o array e conta quantos desses valores são pares.
+
 package main
 
 import "fmt"
 
-func main()  {
+func main() {
+    // Inicializa um array de inteiros com valores de 1 a 10
+    num := [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
-  num := [10]int{1,2,3,4,5,6,7,8,9,10}
-  x := 0
-  for i:=0; i < 10; i++{
-    if num[i] % 2 == 0 {
-      x++ 
+    // Variável para contar o número de elementos pares
+    x := 0
+
+    // Loop para percorrer cada elemento do array
+    for i := 0; i < 10; i++ {
+        // Verifica se o elemento atual é par
+        if num[i] % 2 == 0 {
+            x++ // Incrementa o contador de números pares
+        }
     }
-  }
-  fmt.Println("Numeros pares no arrey: ", x)
+
+    // Imprime o total de números pares encontrados no array
+    fmt.Println("Número de pares no array:", x)
 }
