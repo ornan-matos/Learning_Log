ccd25e9cf22d704c998390693c5212e8bec35acc
Ornan <ornanmatos@outlook.com>
2024-08-18 23:30:38 -0300

    update:adicionar cabeçalho e comentários ao código

--- GO/Basic/arrays/intervalo_em_array.go
+++ GO/Basic/arrays/intervalo_em_array.go
@@ -1,7 +1,19 @@
+// Programa Go que demonstra o uso de slices e arrays
+// Este código cria um array de números de ponto flutuante e, 
+// em seguida, cria um slice que é uma fatia desse array.
+// O slice é então impresso para mostrar os elementos que ele contém.
+
 package main
+
 import "fmt"
+
 func main() {
-arr := [5]float64{1, 2, 3, 4, 5}
-x := arr[0:4]
-  fmt.Println(x)
+    // Criação de um array de 5 elementos do tipo float64
+    arr := [5]float64{1, 2, 3, 4, 5}
+
+    // Criação de um slice que começa no índice 0 e vai até o índice 3 do array (não incluindo o índice 4)
+    x := arr[0:4]
+
+    // Imprime o slice, que deve conter os elementos [1, 2, 3, 4]
+    fmt.Println(x)
 }
