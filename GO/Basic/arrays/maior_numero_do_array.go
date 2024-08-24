cd8e25eff68de7cca208e3363f8d44167010ff1c
Ornan <ornanmatos@outlook.com>
2024-08-23 22:43:24 -0300

    update:adicionar cabeçalho e comentários para explicar o funcionamento do código

--- GO/Basic/arrays/maior_numero_do_array.go
+++ GO/Basic/arrays/maior_numero_do_array.go
@@ -1,18 +1,21 @@
-package main
+#-------------------------------------------------------------------------------------------------------
+# Este código encontra o maior valor em uma lista de números inteiros.
+# O código inicializa uma lista com quatro números, assume o primeiro número como o maior
+# e depois itera pela lista para encontrar o maior valor. Finalmente, imprime o maior valor encontrado.
+#-------------------------------------------------------------------------------------------------------
 
-import "fmt"
+# Inicializa a lista de números
+numeros = [3, 7, 23, 5]
 
-func main() {
-  num := [4]int{3,7,23,5}
-  max := num[0] 
-  
-  for _,value := range num{
-    if max < value {
-      max = value
-    }
-  }
-  fmt.Println(max)
-       
-}
-  
+# Assume que o primeiro número é o maior
+maximo = numeros[0]
 
+# Itera sobre cada valor na lista
+for valor in numeros:
+    # Se o valor atual for maior que o valor máximo conhecido
+    if maximo < valor:
+        # Atualiza o valor máximo
+        maximo = valor
+
+# Imprime o maior valor encontrado
+print(maximo)
