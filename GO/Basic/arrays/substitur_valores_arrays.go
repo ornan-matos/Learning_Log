d44766ba6c5f6ad9c15556dfcdc82cb538549f90
Ornan <ornanmatos@outlook.com>
2024-08-29 11:43:44 -0300

    update:adicionar cabeçalho e cometários explicando o funcionamento do código

--- GO/Basic/arrays/substitur_valores_arrays.go
+++ GO/Basic/arrays/substitur_valores_arrays.go
@@ -2,20 +2,29 @@ package main
 
 import"fmt"
 
+/*
+O programa permite ao usuário substituir um nome de fruta em um array pré-definido. O programa solicita que o usuário insira o índice de um elemento do array e o nome de uma nova fruta. Em seguida, ele substitui a fruta original pelo novo nome no índice indicado e exibe o array atualizado.
+*/
+
 func main(){
   
-  fruit := [3]string{"pera ","uva ","pihna "}
+  // Declara um array de strings com 3 frutas
+  fruit := [3]string{"pera ", "uva ", "pinha "}
+  
+  // Declara uma variável inteira para armazenar o índice escolhido pelo usuário
   var x int
   
+  // Exibe os nomes das frutas originais
   fmt.Println("Nomes originais: ", fruit)
  
+  // Solicita que o usuário insira um índice para escolher qual fruta será alterada
   fmt.Println("\nInsira o número do índice (0 a 2):")
   fmt.Scanf("%d", &x)
   
+  // Solicita que o usuário insira o nome de uma nova fruta
   fmt.Println("\nInsira a nova fruta:")
   fmt.Scanf("%s", &fruit[x])
   
+  // Exibe o array de frutas atualizado com a alteração realizada pelo usuário
   fmt.Println("\nNomes alterados: ", fruit)
-
-
 }
