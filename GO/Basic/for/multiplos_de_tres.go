f3142d57fb163fc8bda780dfc9ba7c2119505ffa
Ornan <ornanmatos@outlook.com>
2024-09-01 23:54:52 -0300

    update:adicionar cabeçalho e comentários ao longo do código

--- GO/Basic/for/multiplos_de_tres.go
+++ GO/Basic/for/multiplos_de_tres.go
@@ -1,14 +1,23 @@
+/*
+Programa em Go para imprimir números de 0 a 99 que são múltiplos de 3.
+Este programa utiliza um loop para iterar de 0 até 99 e verifica se cada número
+é múltiplo de 3. Se for, o número é impresso na tela.
+*/
+
 package main
 
 import "fmt"
 
-func main(){
-  
-  for i := 0; i < 100; i++{
+func main() {
     
-    if i % 3 == 0{
-      fmt.Println(i)
-    } 
-    
-  }
+// Loop que vai de 0 a 99
+    for i := 0; i < 100; i++ {
+        
+	// Verifica se o número atual é múltiplo de 3
+        if i % 3 == 0 {
+            
+	// Imprime o número se for múltiplo de 3
+            fmt.Println(i)
+        }
+    }
 }
