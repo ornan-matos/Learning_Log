7117eff9250fa944ff9cf7e04948b5db1e79b8e8
Ornan <ornanmatos@outlook.com>
2024-09-06 23:27:36 -0300

    update:adicionar cabeçalho e comentários ao longo do código explicando o seu funcionamento

--- GO/Basic/slices/append_em_slices.go
+++ GO/Basic/slices/append_em_slices.go
@@ -1,15 +1,31 @@
 package main
 
-import	"fmt"
+import "fmt"
 
-func main()  {
+/*
+O programa cria um slice de inteiros com capacidade inicial para 3 elementos.
+Em um loop, solicita ao usuário que insira três números, os quais são adicionados ao slice.
+Após o loop, o programa imprime os números inseridos.
+*/
+
+func main() {
+
+  // Cria um slice de inteiros com comprimento 0 e capacidade 3
   num := make([]int, 0, 3)
 
-  for i:=0; i <= 2; i++{
-    var temp int 
+  // Loop para receber 3 números do usuário
+  for i := 0; i <= 2; i++ {
+
+    var temp int
     fmt.Println("Insira um número:")
+
+    // Lê um número inteiro da entrada padrão e armazena em 'temp'
     fmt.Scanf("%d", &temp)
+
+    // Adiciona o número inserido ao slice
     num = append(num, temp)
   }
-  fmt.Println("Os numeros inseridos no slice são:", num)
-} 
+  
+  // Exibe os números armazenados no slice
+  fmt.Println("Os números inseridos no slice são:", num)
+}
