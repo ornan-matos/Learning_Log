117406654d099fbc4701aae5963898d43258fbd6
Ornan <ornanmatos@outlook.com>
2024-09-07 23:07:01 -0300

    feat:adicionar cabeçalho e comentários explicando o funcionamento do código

--- GO/Basic/slices/copy_append.go
+++ GO/Basic/slices/copy_append.go
@@ -2,14 +2,26 @@ package main
 
 import "fmt"
 
+/*
+O código demonstra o uso de slices em Go, incluindo a criação, cópia e concatenação de slices. Cria dois slices e copia os elementos de um para o outro. Concatena um terceiro slice ao slice copiado e imprime o resultado.
+*/
+
 func main() {
-slice1 := []int{1, 2}
-slice2 := make([]int, 2) //make prepara o slice2 para ser escrito
-copy(slice2, slice1) //copia o conteudo do slice1 para o slice2
-
-slice3 := []int{3, 4}
-slice4 := append(slice2, slice3...) //append entre slice2 e slice3
-fmt.Println(slice4)
-
-
+    // Cria um slice 'slice1' com elementos 1 e 2
+    slice1 := []int{1, 2}
+    
+    // Cria um novo slice 'slice2' com tamanho 2, pronto para ser preenchido
+    slice2 := make([]int, 2) 
+    
+    // Copia os elementos de 'slice1' para 'slice2'
+    copy(slice2, slice1) 
+    
+    // Cria um novo slice 'slice3' com elementos 3 e 4
+    slice3 := []int{3, 4}
+    
+    // Concatena 'slice2' com 'slice3' e atribui o resultado a 'slice4'
+    slice4 := append(slice2, slice3...) // O operador '...' é utilizado para expandir 'slice3' como uma lista de argumentos
+    
+    // Imprime o conteúdo de 'slice4', que agora contém os elementos concatenados
+    fmt.Println(slice4)
 }
