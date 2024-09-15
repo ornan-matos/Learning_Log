aea588e32f439189618849f52a7015882af6e1c2
Ornan <ornanmatos@outlook.com>
2024-09-15 14:58:12 -0300

    update:adicionar um cabeçalho e comentários ao longo do código explicando o seu funcionamento

--- GO/Basic/slices/simple_map.go
+++ GO/Basic/slices/simple_map.go
@@ -1,20 +1,37 @@
 package main
+
 import "fmt"
+
+/* 
+
+Este programa demonstra o uso de mapas (map) para armazenar
+e iterar sobre pares chave-valor. Dois mapas são criados: 
+um para armazenar marcas de carros com chaves numéricas, 
+e outro para armazenar nomes pessoais com chaves de string.
+
+*/
+
 func main() {
 
-  marca := make(map[int]string)
+    // Criação de um mapa para associar números a marcas de carros
+    marca := make(map[int]string)
 
-  marca [1] = "FORD" 
-  marca [2] = "MERCEDES"
-  marca [3] = "FIAT"
+    // Adiciona marcas de carros ao mapa "marca"
+    marca[1] = "FORD" 
+    marca[2] = "MERCEDES"
+    marca[3] = "FIAT"
 
-  for _, value := range marca{
-  fmt.Println(value)
-}
-  nome := make(map[string]string)
-  nome["O"] = "Ornan"
-  nome["L"] = "Luiz"
-  
-  fmt.Printf("\nElemento map nome pessoal:\n")
-  fmt.Println(nome["O"])
+    // Itera sobre o mapa "marca" e imprime os valores (marcas de carros)
+    for _, value := range marca {
+        fmt.Println(value)
+    }
+
+    // Criação de um mapa para associar letras a nomes
+    nome := make(map[string]string)
+    nome["O"] = "Ornan"
+    nome["L"] = "Luiz"
+
+    // Imprime o valor associado à chave "O" no mapa "nome"
+    fmt.Printf("\nElemento map nome pessoal:\n")
+    fmt.Println(nome["O"])
 }
