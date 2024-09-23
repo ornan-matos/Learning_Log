68d38b06a455d4b6947dc69ece6de8bb7064d94d
Ornan <ornanmatos@outlook.com>
2024-09-23 20:33:15 -0300

    update:adicionar cabeçalho e comentários explicando o funcionamento do código

--- GO/Basic/Assign_strings_to_a_slice.go
+++ GO/Basic/Assign_strings_to_a_slice.go
@@ -1,25 +1,36 @@
+
 package main
 
 import "fmt"
 
-func main()  {
-  nome := make([]string, 3) // cria um slice com 3 posições
-  
-  for i:=0; i < 3; i++ { // atribui os valores
-    if i == 0 {
-      fmt.Println("Insira o primeiro nome: ") // solicita o nome
-      fmt.Scanf("%s", &nome[i]) // atribui o nome
-    } else if i == 1 { // verifica se é o segundo nome
-      fmt.Println("Insira o segundo nome: ") // solicita o segundo nome
-      fmt.Scanf("%s", &nome[i]) 
+/*
+O programa solicita ao usuário que insira três nomes e os armazena em um slice. 
+Após a coleta, ele exibe os nomes inseridos.
+*/
 
-    } else if i == 2 {
-      fmt.Println("Insira o terceiro nome: ")
-      fmt.Scanf("%s", &nome[i])
+func main() {
+    // Cria um slice chamado 'nome' com 3 posições para armazenar os nomes.
+    nome := make([]string, 3) 
+    
+    // Loop para solicitar os nomes ao usuário.
+    for i := 0; i < 3; i++ { 
+        // Verifica qual nome está sendo solicitado.
+        if i == 0 {
+            fmt.Println("Insira o primeiro nome: ") // Solicita o primeiro nome
+            fmt.Scanf("%s", &nome[i]) // Lê o nome e o armazena no slice
+        } else if i == 1 { // Verifica se é o segundo nome
+            fmt.Println("Insira o segundo nome: ") // Solicita o segundo nome
+            fmt.Scanf("%s", &nome[i]) // Lê o nome e o armazena no slice
+        } else if i == 2 { // Verifica se é o terceiro nome
+            fmt.Println("Insira o terceiro nome: ") // Solicita o terceiro nome
+            fmt.Scanf("%s", &nome[i]) // Lê o nome e o armazena no slice
+        }
     }
-  }
-  for x:=0; x < len(nome); x++{ //exibe os nomes
-    fmt.Printf("%s ", nome[x])
-  }
-  fmt.Println()
+    
+    // Loop para exibir os nomes armazenados no slice.
+    for x := 0; x < len(nome); x++ { 
+        fmt.Printf("%s ", nome[x]) // Exibe cada nome
+    }
+    
+    fmt.Println() // Adiciona uma nova linha após a lista de nomes
 }
