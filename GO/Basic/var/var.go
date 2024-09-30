ba03e4e5239a3cb1ad15669f465fd17b98b927a8
Ornan <ornanmatos@outlook.com>
2024-09-29 21:43:12 -0300

    update:adicionar cabeçalho e comentários explicando o funcionamento do código

--- GO/Basic/var/var.go
+++ GO/Basic/var/var.go
@@ -1,16 +1,29 @@
+
 package main
 
 import "fmt"
 
-func main() {
+/*
+Este programa simples em Go exibe uma mensagem de boas-vindas ao usuário,
+verifica e exibe a idade do usuário e mostra a versão do programa.
+*/
 
-	var nome string = "Ornan"
-	var versao float32 = 1.2
-	var idade int = 26
+func main() {
+    // Declaração de variáveis
+	var nome string = "Ornan"    // Nome do usuário
+	var versao float32 = 1.2      // Versão do programa
+	var idade int = 26             // Idade do usuário
+	
+	// Mensagem de boas-vindas
 	fmt.Println("Olá", nome, "bem-vindo ao nosso terminal")
+	
+	// Mensagem informando que a idade está sendo verificada
 	fmt.Println("Verificando a sua idade em nossa Database.")
 	fmt.Println("...")
+	
+	// Exibe a idade do usuário
 	fmt.Println("A sua idade é", idade, "anos.")
+	
+	// Exibe a versão do programa
 	fmt.Println("Versão:", versao)
-
 }
