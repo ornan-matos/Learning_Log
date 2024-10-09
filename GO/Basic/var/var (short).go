62b3ac537608c3003cacf284b328e470bb429baf
Ornan <ornanmatos@outlook.com>
2024-10-08 21:10:59 -0300

    update:adicionar cabeçalho explicando o funcionamento do código

--- GO/Basic/var/var (short).go
+++ GO/Basic/var/var (short).go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+/*
+Programa simples que exibe uma variável inteira, uma variável de ponto flutuante e imprime um string na tela.
+*/
+
 func main() {
 
 	nome := "Ornan Matos"
