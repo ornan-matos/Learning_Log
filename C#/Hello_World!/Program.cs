745454e2a40849d3fbbdbb6a7d5bc04314be9248
ornan-matos <ornanmatos@outlook.com>
2025-01-25 18:27:45 -0300

    feature:primeiro código em C#

---
+++ C#/Hello_World!/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/*
+ Hello World!
+ */
+
+class program{
+  static void Main(){
+
+
+    //WriteLine -> Quebra a Linha
+    Console.WriteLine("Hello world!");
+
+    //Write -> Não Quebra Linha
+    Console.Write("Hello Plus++\n"); 
+    //Para quebra a linha nesse caso adicione \n
+  }
+
+}
