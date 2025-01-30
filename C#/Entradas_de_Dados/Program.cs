fd46c5483a5ef0b59e542bb87ace0a80fe7629f6
ornan-matos <ornanmatos@outlook.com>
2025-01-30 08:23:27 -0300

    feature:entrada de dados

---
+++ C#/Entradas_de_Dados/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+class entrada{
+  static void Main(){
+    Console.WriteLine("Digite seu nome:");
+    string res = Console.ReadLine();
+    Console.WriteLine($"Obrigado, {res}!");
+  }
+}
