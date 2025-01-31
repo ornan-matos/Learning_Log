46519276d4066f021b04a151899e7d45eee44fa8
ornan-matos <ornanmatos@outlook.com>
2025-01-31 09:32:38 -0300

    feature:Tratamento de Strings

---
+++ C#/Tratamento_de_Strings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class tratamento{
+  static void Main(){
+    //Tratamento de Strings
+
+    //Formatação de String
+    Console.WriteLine("Insira um texto para converte-lo:");
+    string frase = Console.ReadLine();
+
+    Console.WriteLine("\nCaixa Alta:");
+    Console.WriteLine(frase.ToUpper());
+
+    Console.WriteLine("\nCaixa Baixa:");
+    Console.WriteLine(frase.ToLower());
+
+    Console.WriteLine("\nInsira seu nome:");
+    string nome = Console.ReadLine();
+    Console.WriteLine(String.Format("\nOlá {0}, bem vindo!", nome));
+  }
+}
