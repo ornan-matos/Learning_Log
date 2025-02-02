b24aa85bf450f47766689b694ec75da3abee3a66
ornan-matos <ornanmatos@outlook.com>
2025-02-02 11:21:32 -0300

    feature:area do quadrilatero

---
+++ C#/Area_Quadrilatero/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class area{
+  static void Main(){
+
+    Console.WriteLine("\nCalcule a Área de um Quadrilátero(Retângular)");
+
+    Console.WriteLine("\nInsira a Altura do Retângulo:");
+    float altura = Convert.ToSingle(Console.ReadLine());
+
+    Console.WriteLine("\nInsira a Base do Retângulo:");
+    float base_ = Convert.ToSingle(Console.ReadLine());
+
+    float area = base_ * altura;
+
+    Console.WriteLine("\nA área do Quadrilátero = " + area);
+
+  }
+}
