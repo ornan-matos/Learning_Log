56291aba75af9bfbaee68eff59ed745e470a2f65
ornan-matos <ornanmatos@outlook.com>
2025-02-02 11:08:04 -0300

    feature:metros em centimetros e milimetros

---
+++ C#/Metros_em_CM/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class conversao{
+  static void Main(){
+    Console.WriteLine("\nConversão de Metros em Centímetros");
+    Console.WriteLine("\nInsira a Quantidade de Metros");
+
+    float metros = Convert.ToSingle(Console.ReadLine());
+
+    float centimetros = metros * 100;
+    float milimetros = metros * 1000;
+
+    Console.WriteLine(String.Format("\n{0}M\n=\n{1}cm\n{2}mm", metros, centimetros, milimetros));
+  }
+}
