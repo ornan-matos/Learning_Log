6681c2ad24b4d3e7b8cb564c7138e78a4ee86b71
ornan-matos <ornanmatos@outlook.com>
2025-02-01 23:54:21 -0300

    feature:exercício media de notas

---
+++ C#/Media_Notas/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class media{
+  static void Main(){
+    Console.WriteLine("Media Notas");
+    
+    Console.WriteLine("\nMatématica");
+    float nota1 = Convert.ToSingle(Console.ReadLine());
+
+    Console.WriteLine("\nPortuguês");
+    float nota2 = Convert.ToSingle(Console.ReadLine());
+
+
+    Console.WriteLine("\nBiologia");
+    float nota3 = Convert.ToSingle(Console.ReadLine());
+
+    Console.WriteLine("\nGeografia");
+    float nota4 = Convert.ToSingle(Console.ReadLine());
+
+    float media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+    Console.WriteLine("\nResultado Media: " + media);
+
+  }
+}
