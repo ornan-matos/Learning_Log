054c81d399b8077fe8ab78f8e04ade30150def40
ornan-matos <ornan.matos@fbest.org.br>
2025-02-03 17:41:58 -0300

    feature:Construir conversor

---
+++ C#/Cotação_Dolar/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class cota{
+  static void Main(){
+    Console.WriteLine("\nConversor U$ -> R$\n");
+
+    Console.Write("\nCotação Dolar: U$ ");
+    float cotacao = Convert.ToSingle(Console.Read());
+
+    Console.Write("\nValor para Converter: U$ ");
+    float valor = Convert.ToSingle(Console.Read());
+
+    float dolar = cotacao * valor;
+
+    Console.WriteLine()
+  }
+}
