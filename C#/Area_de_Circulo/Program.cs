6341948a403605611a30e5148d6bd0f698c31dff
ornan-matos <ornan.matos@fbest.org.br>
2025-02-03 12:34:22 -0300

    feature:Calculo de area de circulo

---
+++ C#/Area_de_Circulo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class area{
+  static void Main(){
+    Console.WriteLine("\nCálculo de Área de Circulo em Centímetros\n");
+
+    Console.WriteLine("\nInsira o raiodo do cirulo (em cm)");
+
+    double raio = Convert.ToDouble(Console.ReadLine());
+
+    double area = 3.14 * (Math.Pow(raio,2));
+
+    Console.WriteLine(String.Format("\nArea Corespondente: {0}cm", area));
+
+  }
+}
