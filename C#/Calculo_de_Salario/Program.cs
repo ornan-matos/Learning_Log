828fab8ed5fc3b3f12cfb6c7c3a79b899dd65033
ornan-matos <ornanmatos@outlook.com>
2025-02-03 08:34:29 -0300

    feature:Calcular ganhos mensais

---
+++ C#/Calculo_de_Salario/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class salario{
+
+  static void Main(){
+
+    Console.WriteLine("\nCalculo de Salário\n");
+
+    Console.WriteLine("\nQuantas horas você trabalha por semana:");
+    int horas_sem = Convert.ToInt16(Console.ReadLine());
+    
+    Console.WriteLine("\nQuanto você ganha por hora:");
+    int valor_hora = Convert.ToInt16(Console.ReadLine());
+
+    float salario = valor_hora * (horas_sem * 4); 
+
+    Console.WriteLine(String.Format("\nGanhos mensais:\nR$ {0}",salario ));
+
+  }
+}
