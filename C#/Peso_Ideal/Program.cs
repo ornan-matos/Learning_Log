aa18cabe5d72b7e7d1dcff89075c92db5f08de8e
ornan-matos <ornanmatos@outlook.com>
2025-02-03 08:18:37 -0300

    feature:Calcular peso recomendado

---
+++ C#/Peso_Ideal/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class peso{
+  static void Main(){
+    Console.WriteLine("\nCálculo do Peso Recomendado\n");
+
+    Console.WriteLine("\nQual o seu nome:");
+    string nome = Console.ReadLine();
+
+    Console.WriteLine("\nQual a sua altura? (em metros)");
+    float altura = Convert.ToSingle(Console.ReadLine());
+
+    float peso = (72.7f * altura) - 58;
+
+    Console.WriteLine(String.Format("\nOlá {0}! O seu peso recomendado é {1:F2}kg", nome, peso));
+
+
+
+  }
+}
