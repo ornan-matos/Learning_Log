4a1e1621dfc4480bd232d11e73d8879b72767fc0
ornan-matos <ornan.matos@fbest.org.br>
2025-02-03 15:18:15 -0300

    feature:Media de estoque recomendado

---
+++ C#/Estoque_de_Peças/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class media{
+  static void Main(){
+    Console.WriteLine("\nEstoque Médio de Parafusos\n");
+    
+    Console.WriteLine("\nIsira o número maxímo de Parafusos suportados pelo estoque:");
+    int max = Convert.ToInt32(Console.ReadLine());
+    
+    Console.WriteLine("\nIsira o número minimo de Parafusos que o estoque deve conter:");
+    int min = Convert.ToInt32(Console.ReadLine());
+
+    int media = (min + max) / 2;
+
+    Console.WriteLine("\nEstoque medio recomendado: " + media + "parafusos.");
+
+  }
+}
