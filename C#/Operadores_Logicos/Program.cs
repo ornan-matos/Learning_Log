fe749b7a77b1d962ef83ab346c76ade6c0f291df
ornan-matos <ornanmatos@outlook.com>
2025-02-04 09:44:20 -0300

    feature:Operadore Lógicos

---
+++ C#/Operadores_Logicos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class operadores{
+  static void Main(){
+    
+    bool b1 = true;
+    bool b2 = true;
+    bool b3 = false;
+    bool b4 = false;
+
+    bool exp = b3 && b4; //AND (e)
+    
+    bool exp2 = b1 || b3; //OR (ou)
+
+    bool exp3 = b4 || b3; //OR (ou)
+
+    bool exp4 = b1 == b2 && b3 == b4;
+
+    Console.WriteLine($"{exp}, {exp2}, {exp3}, {exp4}");
+
+    Console.WriteLine($"{!b1}, {!b3}");
+
+    Console.WriteLine(!(b1 == b2));
+
+  }
+}
