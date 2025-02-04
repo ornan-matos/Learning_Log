d5b9bc333d64792c15a87c869f37bc444f3489fd
ornan-matos <ornan.matos@fbest.org.br>
2025-02-04 13:50:10 -0300

    feature:calculo de litros

---
+++ C#/Litros/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class litros{
+  static void Main(){
+    Console.WriteLine("Forneça o tempo médio da viagem:");
+    float tempo = Convert.ToSingle(Console.ReadLine());
+
+    Console.WriteLine("Forneça a velocida média:");
+    int velocida = Convert.ToInt32(Console.ReadLine());
+
+    int distancia = Convert.ToInt32(tempo) * velocida;
+
+    int litros = distancia / 12;
+
+    Console.WriteLine(String.Format("\nA "));
+
+
+  }
+}
