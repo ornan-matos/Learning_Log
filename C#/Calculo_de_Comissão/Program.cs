1bf93ac1889278c41fc6e3d45efab85fbe51a138
ornan-matos <ornanmatos@outlook.com>
2025-02-04 08:39:36 -0300

    feature:calculo de porcentagem

---
+++ C#/Calculo_de_Comissão/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class comissao{
+  static void Main(){
+
+    Console.WriteLine("\nNome do Vendedor:");
+    string nome = Console.ReadLine();
+
+    Console.WriteLine("\nCódigo:");
+    int codigo = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("\nPreço: R$ ");
+    float preco = Convert.ToSingle(Console.ReadLine());
+
+    Console.WriteLine("\nQuantidade: ");
+    int quantidade = Convert.ToInt32(Console.ReadLine());
+
+    float total = (preco * quantidade) * 0.05f;
+
+    Console.WriteLine(String.Format("\n{0} você vendeu {1} peça(s)\nComissão: R${2:F2}", nome, quantidade, total));
+  }
+}
