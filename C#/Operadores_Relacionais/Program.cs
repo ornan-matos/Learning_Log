736800e23bf5e0e52e4674057d751d52ad4ff971
ornan-matos <ornanmatos@outlook.com>
2025-02-04 09:29:18 -0300

    feature:Operadore Relacionais

---
+++ C#/Operadores_Relacionais/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class operadores{
+  static void Main(){
+    //Operadores Relacionais
+    int n1 = 10;
+    int n2 = 20;
+    int n3 = 10;
+
+    bool v1 = n1 == n3; //Igualdade
+    bool v2 = n1 != n2; //Diferença
+
+    bool v3 = n1 > n2; //Maior que
+    bool v4 = n3 < n2; //Menor que
+
+    bool v5 = n1 >= n3; //Maior ou igual
+    bool v6 = n2 <= n1; //Menor ou igual
+
+    Console.WriteLine(v5);
+
+  }
+}
