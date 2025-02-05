0925fc7a2afcaa97ede0cfcbe9fd2d70d3e9b143
ornan-matos <ornanmatos@outlook.com>
2025-02-05 13:08:30 -0300

    feature:Estrutura condicional multipla usando temperatura

---
+++ C#/Estrutura_Condicional_Multipla/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class multipla{
+  static void Main(){
+
+    Console.Write("\nTemperatura atual (Celcius):");
+    Int16 temperatura = Convert.ToInt16(Console.ReadLine());
+
+    if (temperatura > 25 && temperatura < 29){
+      Console.WriteLine("\nTemperatura agradavel!");
+    }
+    else if (temperatura == 25){
+      Console.WriteLine("\nTemperatura ambiente!");
+    }
+    else if (temperatura < 25 && temperatura > 15){
+      Console.WriteLine("\nTempo Frio!");
+    }
+    else if (temperatura >= 29){
+      Console.WriteLine("\nTempo Quente!");
+    }
+    else{
+      Console.WriteLine("\nTempo Muito Frio!");
+    }
+
+  }
+}
