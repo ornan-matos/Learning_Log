3c8b171f66664c4443c5e05f68cb2db26e231d21
ornan-matos <ornanmatos@outlook.com>
2025-02-05 12:49:04 -0300

    feature:Estrutura condicional

---
+++ C#/Estrutura_Condicional/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class condicional{
+  static void Main(){
+    Console.Write("\nInsira sua idade: ");
+    Int16 idade = Convert.ToInt16(Console.ReadLine());
+    
+    if (idade >= 18){
+      Console.WriteLine("\nVenda autorizada!");
+    }
+    else{
+      Console.WriteLine("\nVenda proibida!"); }
+  }
+}
