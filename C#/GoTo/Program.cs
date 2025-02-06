69f14afb8c286f784cf66ccec286040ffc42b626
ornan-matos <ornanmatos@outlook.com>
2025-02-06 15:03:40 -0300

    feature:utilizar indices e GoTo

---
+++ C#/GoTo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class go_to{
+  
+  static void Main(){
+
+    inicio:
+
+    Console.Write("\nDigite um número: (0-10): ");
+    int n1 = Convert.ToInt32(Console.ReadLine());
+
+    if (n1 >= 0 && n1 <= 10){
+      Console.WriteLine("\nValor entregue!");
+    } else{
+      Console.WriteLine("\nValor não reconhecido! Tente novamente...");  
+      goto inicio;
+    }
+
+  }
+}
