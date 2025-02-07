17c76cd5512698a4337040fa65c28fdf4206150b
ornan-matos <ornanmatos@outlook.com>
2025-02-07 11:04:21 -0300

    feature:maior número entre dois

---
+++ C#/Maior_de_Dois/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class maior{
+  static void Main(){
+    Console.WriteLine("\nMaior de dois números");
+
+    Console.Write("\nInsira o primeiro número: ");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+    
+    Console.Write("\nInsira o segundo número: ");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+
+    if (num1 > num2){
+
+      Console.WriteLine(String.Format("\nO número {0} é o maior.", num1));
+
+    } else if (num1 < num2){
+
+      Console.WriteLine(String.Format("\nO número {0} é o maior.", num2));
+
+    } else {
+
+      Console.WriteLine(String.Format("\nOs números {0}, {1} são iguais!", num1, num2));
+
+    }
+
+  }
+}
