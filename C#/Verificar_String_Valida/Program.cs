12f0a84b0c6eb33713af9462a026fe69ed0c54d7
ornan-matos <ornanmatos@outlook.com>
2025-02-07 11:22:40 -0300

    feature:verificar string

---
+++ C#/Verificar_String_Valida/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class valido{
+
+  static void Main(){
+    Console.WriteLine("\nSeleção de Sexo");
+
+inicio:
+
+    Console.WriteLine("\nM...\tMasculino");
+    Console.WriteLine("F...\tFeminino\n");
+
+    string sx = Console.ReadLine();
+
+    if (sx == "M"){
+      Console.WriteLine("\nVocê selecionou Masculino");
+    } else if (sx == "F"){
+      Console.WriteLine("\nVocê selecionou Feminino");
+    } else{
+      Console.WriteLine("\nSeleção Invalida! Tente novamente.");
+      goto inicio;
+    }
+    
+  }
+}
