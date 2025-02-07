54883146860eef8d901ced86e3c5ac8610ccf4fc
ornan-matos <ornanmatos@outlook.com>
2025-02-07 11:13:24 -0300

    feature:verificar número positivo e negativo

---
+++ C#/Positivo_ou_Negativo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class p_ou_v{
+  static void Main(){
+    Console.WriteLine("\nPositivo ou Negativo");
+
+inicio:
+    Console.Write("\nInsira um valor: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+
+    if (num > 0){
+      Console.WriteLine(String.Format("\n{0} é positivo.", num));
+    } else if (num < 0){
+      Console.WriteLine(String.Format("\n{0} é negativo.", num));
+    } else {
+      Console.WriteLine(String.Format("\n{0} é neutro. Tente novamente", num));
+      goto inicio;
+    }
+  }
+}
