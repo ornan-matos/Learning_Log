0ed69ec4ef02adb94faca206e336d0d807c4e88b
ornan-matos <ornanmatos@outlook.com>
2025-02-07 09:37:45 -0300

    feature:verificar tipos de variaveis

---
+++ C#/Verificar_Variáveis/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class verificar{
+  static void Main(){
+    Console.Write("\nInsira um número: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+
+    if (num.GetType() == typeof(Int32)){
+      Console.WriteLine("\nÉ um número inteiro! (" + num.GetType() +")");
+    } else{
+      Console.WriteLine("\nNão é do mesmo formato!");
+    }
+  }
+}
