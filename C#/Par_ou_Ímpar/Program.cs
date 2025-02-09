ff2dee23e4b04f39576045e13af1f65a01bae467
ornan-matos <ornanmatos@outlook.com>
2025-02-09 18:59:25 -0300

    feature:determinar se um número é par ou impar

---
+++ C#/Par_ou_Ímpar/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class par_impar{
+  static void Main(){
+
+    Console.Write("\nInsira um número: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+
+    if (num % 2 == 0){
+      Console.WriteLine("\nO número é par!");
+    } else {
+      Console.WriteLine("\nO número é impar!");
+    }
+  }
+}
