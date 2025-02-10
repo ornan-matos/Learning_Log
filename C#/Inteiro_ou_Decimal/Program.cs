b98777632d45eded9d818816a34a268b0baef975
ornan-matos <ornanmatos@outlook.com>
2025-02-10 10:49:55 -0300

    fetaure:definir se um número é inteiro ou decimal

---
+++ C#/Inteiro_ou_Decimal/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class int_ou_dec{
+  static void Main(){
+    Console.Write("\nInsira um número (inteiro ou decimal): ");
+    float num = Convert.ToSingle(Console.ReadLine());
+
+    if (num % 1 == 0){
+      Console.WriteLine("\nO número é inteiro!");
+    } else {
+      Console.WriteLine("\nO número é decimal!");
+    }
+  }
+}
