a15a305e0be23926f25ccd498b311976f97da553
ornan-matos <ornanmatos@outlook.com>
2025-02-10 16:07:12 -0300

    fetaure:estrutura de incremento e decremento

---
+++ C#/Incremento_Decremento/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class inc{
+  static void Main(){
+    //Incremento
+    int n1 = 10;
+    Console.WriteLine(++n1);//Prefixo
+    //Decremento
+    Console.WriteLine(n1--);//Sufixo
+    //Incremento
+    Console.WriteLine(n1++);//Sufixo
+    //Decremento
+    Console.WriteLine(--n1);//Prefixo
+  }
+}
