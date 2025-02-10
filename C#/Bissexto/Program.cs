074f766b27e20cc0a27121da422706a6cad46af9
ornan-matos <ornanmatos@outlook.com>
2025-02-10 11:42:07 -0300

    fetaure:verificar se o ano é bissexto

---
+++ C#/Bissexto/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class bissexto{
+    static void Main(){
+      Console.WriteLine("\nDescubra se o ano é Bissexto\n");
+
+      Console.Write("Insira o ano: ");
+      int ano = Convert.ToInt32(Console.ReadLine());
+
+      if (ano % 4 == 0 && ano % 100 == 0 && ano % 400 == 0){
+        Console.WriteLine(String.Format("\n{0} é bissexto!", ano));
+      } else {
+        Console.WriteLine(String.Format("\n{0} não é bissexto!", ano));
+      }
+    }
+}
