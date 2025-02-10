d6f3587db709df5b1abc7f1338143ec7a10ef686
ornan-matos <ornanmatos@outlook.com>
2025-02-10 12:04:09 -0300

    fetaure:verificar o maior número interio entre três

---
+++ C#/Maior_de_Três/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class maior_três{
+  static void Main(){
+    Console.WriteLine("\nMaior de Três números");
+
+    Console.Write("\nInsira o primeiro número: ");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("\nInsira o segundo número: ");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("\nInsira o terceiro número: ");
+    int num3 = Convert.ToInt32(Console.ReadLine());
+
+    if (num1 > num2 && num1 > num3){
+
+      Console.WriteLine(String.Format("\n{0} é o maior número.", num1));
+
+    } else if (num2 > num1 && num2 > num3){
+
+      Console.WriteLine(String.Format("\n{0} é o maior número.", num2));
+
+    } else if (num3 > num1 && num3 > num2) {
+
+      Console.WriteLine(String.Format("\n{0} é o maior número.", num3));
+
+    }  
+  }
+}
