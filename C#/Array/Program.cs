001645fa1051ebcd7f5ec97e5c00e4cb9cf842d5
ornan-matos <ornanmatos@outlook.com>
2025-02-11 10:32:16 -0300

    feature:utilizar array atraves de um loop for

---
+++ C#/Array/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Array_{
+  static void Main(){
+
+    int[] Vetor1 = new int[4]{10,20,30,50};
+
+    Vetor1[3] = 40;
+
+    for (int i = 0; i < Vetor1.Length; i++){
+      Console.WriteLine(Vetor1[i]);
+    }
+  }
+}
