59aab5c1f4f5758180531e39dbe61dbba297ecd7
ornan-matos <ornanmatos@outlook.com>
2025-02-11 09:15:56 -0300

    feature:utilizar break e continue

---
+++ C#/Break_Continue/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class break_continue{
+  static void Main(){
+    int num = 0;
+
+    while (num <= 10){
+      if(num % 2 == 1){
+        //Num impar
+        num++;
+        continue;
+      }
+      Console.WriteLine(num);
+      num++;
+    }
+
+    while (num <= 20){
+      
+      num++;
+      
+      if (num == 15){
+        break;
+      }
+      Console.WriteLine(num);
+    }
+  }
+}
