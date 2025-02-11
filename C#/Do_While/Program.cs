2286cee314d9448280c7c914999b042975c8db0a
ornan-matos <ornanmatos@outlook.com>
2025-02-11 09:55:48 -0300

    feature:utilizar do_while para escutar e em seguida testar

---
+++ C#/Do_While/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class do_while{
+  static void Main(){
+    
+    int n1 = 0;
+
+    do{
+      
+      n1++;
+      
+      Console.WriteLine(n1);
+
+    } while(n1 < 10);
+  }
+}
