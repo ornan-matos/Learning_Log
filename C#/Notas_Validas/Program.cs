2f36f2d097820448b2f1e711d4070fb7ce75cb6e
ornan-matos <ornanmatos@outlook.com>
2025-02-12 10:51:08 -0300

    feature:notas validas através de uma estrutura de decisão simples

---
+++ C#/Notas_Validas/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class notas{
+  static void Main(){
+
+inicio:
+    Console.Write("\nInsira nota (0 a 10): ");
+    float nota = Convert.ToSingle(Console.ReadLine());
+
+    if (nota >= 0 && nota <= 10){
+      Console.WriteLine("\nValor valido!");
+    } else {
+      Console.WriteLine("\nValor invalido! Tente novamente!");
+      goto inicio;
+    } 
+  }
+
+}
