5fa95710762e95a1f43a357c434f187a14d1d289
ornan-matos <ornanmatos@outlook.com>
2025-02-12 11:45:41 -0300

    feature:exibir intervalos de número inteiros através de input do usuário

---
+++ C#/Intervalo_Números/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+ 
+class intervalo
+{
+    static void Main() {
+ 
+        Console.Write("\nInsira o primeiro número: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+ 
+        Console.Write("\nInsira o segundo número: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+ 
+ 
+ 
+        for (int diferenca = num1 - num2; diferenca <= 0; diferenca--){
+            if (num1 <= num2)
+            {
+                Console.WriteLine(num1);
+                num1++;
+            }
+        }
+    }
+}
