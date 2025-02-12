b0de9046074700382924d3f0245e53e43ef1a703
ornan-matos <ornanmatos@outlook.com>
2025-02-12 11:22:43 -0300

    feature:contar números impares usando estrutura de repetição e estrutura de decisão

---
+++ C#/Números_Ímpares/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class impare{
+  static void Main(){
+
+    for(int num = 1; num <= 50; num++){
+
+      if (num % 2 == 1){
+        Console.WriteLine(num);
+      }
+    }
+  }
+}
+
