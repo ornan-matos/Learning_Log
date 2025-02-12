b62b995a49b613a7a1e162aea1cfc68abdb838a6
ornan-matos <ornanmatos@outlook.com>
2025-02-12 10:42:41 -0300

    feature:adicionar metodos para manipular arrays

---
+++ C#/Metodos_Arrays_e_Matrizes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class metodos{
+  static void Main(){
+    int[] vetor = {1,2,3,4,5};
+
+    //Posição do valor definido
+    int pos = Array.BinarySearch(vetor, 3);
+    Console.WriteLine(pos);
+    
+    //Conteúdo da posição definida
+    object pos2 = vetor.GetValue(3);
+    Console.WriteLine(pos2);
+
+    //Menor valor do index do array
+    int min_index = vetor.GetLowerBound(0);
+    //Maior valor do index do array
+    int max_index = vetor.GetUpperBound(0);
+
+    Console.WriteLine(String.Format("\nMenor valor do index:{0}\nMaior valor do index:{1}\n", min_index, max_index));
+
+    Array.Reverse(vetor);
+    foreach(int n in vetor){
+      Console.WriteLine(n);
+    }
+
+  }
+}
