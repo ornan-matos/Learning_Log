7d50bf83a5e5c750cf1e3827317cc2053b34db59
ornan-matos <ornanmatos@outlook.com>
2025-02-13 11:32:43 -0300

    feature:calcular dias de vida

---
+++ C#/Calculo_de_idade/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class idade{
+  static void Main(){
+
+    Console.WriteLine("\nInsira a quantidade de anos, meses e dias que você possui.");
+    
+    Console.Write("\nAnos: ");
+    int anos = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("\nMeses: ");
+    int meses = Convert.ToInt32(Console.ReadLine());
+    
+    Console.Write("\nDias: ");
+    int dias = Convert.ToInt32(Console.ReadLine());
+    
+    int idade = ((anos * 365) + (meses * 30)) + dias; 
+
+    Console.WriteLine(String.Format("\nTotal de dias de vida: {0} dias", idade));    
+
+  }
+}
