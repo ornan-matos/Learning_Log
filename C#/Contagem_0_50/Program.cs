8e4db9f64df66fce780244266306831357e83352
ornan-matos <ornanmatos@outlook.com>
2025-02-13 09:34:25 -0300

    feature:contar de array de 0 a 50

---
+++ C#/Contagem_0_50/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class contagem{
+  static void Main(){
+
+    int[] vetor = new int[51];
+
+    for (int n = 0; n < vetor.Length; n++){
+      vetor[n] = n;
+      Console.WriteLine(vetor[n]);
+    }
+  }
+}
