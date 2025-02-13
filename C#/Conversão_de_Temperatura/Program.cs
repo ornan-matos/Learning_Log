ea115b571059fb8af554f41881e83f4fa865e030
ornan-matos <ornanmatos@outlook.com>
2025-02-13 11:11:16 -0300

    feature:converte celsius em fahrenheit e também de forma inversa

---
+++ C#/Conversão_de_Temperatura/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class conversao{
+  static void Main(){
+
+    Console.WriteLine("\nConverção de Celsius para Fahrenheit");
+
+    Console.Write("\nInsira a temperatura em Celsius: ");
+    float celsius = Convert.ToSingle(Console.ReadLine());
+
+    float fahrenheit = (9 * celsius + 160) / 5;
+
+    Console.WriteLine(String.Format("{0} ºC\n{1} ºF", celsius, fahrenheit));
+
+    Console.WriteLine("--------------------------");
+
+    Console.WriteLine("\nConverção de Fahrenheit para Celsius");
+    
+    Console.Write("\nInsira a temperatura em Fahrenheit: ");
+    float fahrenheit2 = Convert.ToSingle(Console.ReadLine());
+
+    float celsius2 = (fahrenheit2 - 32) * 5 / 9;
+
+    Console.WriteLine(String.Format("{0} ºF\n{1} ºC", fahrenheit2, celsius2));
+
+  }
+}
