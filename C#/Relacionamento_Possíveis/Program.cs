e6a3956618517fb125c14213c4fa9758ace5e18f
ornan-matos <ornanmatos@outlook.com>
2025-02-13 11:43:55 -0300

    feature:adicioar possibilidades entre números através de estrutura de decisão

---
+++ C#/Relacionamento_Possíveis/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class realcionamento{
+  static void Main(){
+    Console.Write("\nInsira o primeiro número: ");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+    
+    Console.Write("\nInsira o segundo número: ");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+
+    if (num1 == num2){
+      Console.WriteLine("\nOs número são iguais.");
+    } 
+
+    else {
+      Console.WriteLine("\nOs número são diferentes.");
+
+      if (num1 > num2){
+        Console.WriteLine(String.Format("\n{0} é maior que {1}", num1, num2));
+      }
+      else if (num1 < num2){
+        Console.WriteLine(String.Format("\n{0} é menor que {1}", num1, num2));
+      }
+    } 
+
+  }
+}
