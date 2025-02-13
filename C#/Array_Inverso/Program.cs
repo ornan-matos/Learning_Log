bd71720729dc1cccc37f06607a0db328cb4d95eb
ornan-matos <ornanmatos@outlook.com>
2025-02-13 09:28:51 -0300

    feature:inverter sequencia de arrays fornecidos por input

---
+++ C#/Array_Inverso/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class inverso{
+  static void Main(){
+
+    int[] vetor = new int[10];
+
+    for (int n = 0; n < vetor.Length; n++){
+      Console.Write("\nInsira um número: ");
+      vetor[n] = Convert.ToInt32(Console.ReadLine());
+    }
+
+    Array.Reverse(vetor);
+    foreach (int n in vetor){
+      Console.WriteLine("\n" + n);
+    }
+  }
+}
