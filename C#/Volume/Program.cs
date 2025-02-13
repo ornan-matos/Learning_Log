32ae3a437fadebe99503d9b4b6b792ab05afd7b7
ornan-matos <ornanmatos@outlook.com>
2025-02-13 11:23:03 -0300

    feature:calcular centimetros cubicos

---
+++ C#/Volume/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class volume{
+  static void Main(){
+  
+    Console.WriteLine("\nCalulo de Volume");
+
+    Console.Write("\nInsira raio (cm): ");
+    float raio = Convert.ToSingle(Console.ReadLine());
+    
+    Console.Write("\nInsira altura (cm): ");
+    float altura = Convert.ToSingle(Console.ReadLine());
+
+    double volume = 3.14159 * raio * raio * altura;
+
+    Console.WriteLine(String.Format("\nVolume = {0}cm³", volume));
+
+
+
+
+  }
+}
