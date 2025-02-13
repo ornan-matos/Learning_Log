5f90b3cf56a70d938bb7ea1833866906079471b8
ornan-matos <ornanmatos@outlook.com>
2025-02-13 08:36:28 -0300

    feature:inserir dados em posições no array

---
+++ C#/Ler_Vetor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class ler{
+  static void Main(){
+
+    int[] vetor = new int[5];
+
+    for (int n = 0; n < vetor.Length; n++){
+
+      Console.Write("\nInsira um número: ");
+      vetor[n] = Convert.ToInt32(Console.ReadLine());
+
+    }
+
+    foreach (int elemento in vetor){
+
+      Console.WriteLine("\n" + elemento);
+
+    }
+
+  }
+}
