aace97e1cc9ed1cbd0c7d62f2aeffb2cedb0be01
ornan-matos <ornanmatos@outlook.com>
2025-02-14 10:08:01 -0300

    update:ajustar logica na estrutura de decisão de ordem numerica

---
+++ C#/Ordem_Decresente/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class decresente{
+  static void Main(){
+
+    int[] num = new int[3];
+
+    for (int n = 0; n < num.Length; n++){
+
+      Console.Write("\nInsira o primeiro número: ");
+      num[n] = Convert.ToInt32(Console.ReadLine());
+
+    }
+    
+
+    if (num[0] >= num[1] && num[0] >= num[2]){
+      Console.WriteLine("\n" + num[0]);
+      if (num[1] >= num[2]){
+        Console.WriteLine("\n" + num[1]);
+        Console.WriteLine("\n" + num[2]);
+      }
+      else {
+        Console.WriteLine("\n" + num[2]);
+        Console.WriteLine("\n" + num[1]);
+      }
+    }
+    else if (num[1] >= num[0] && num[1] >= num[2]){
+      Console.WriteLine("\n" + num[1]);
+      if (num[0] >= num[2]){
+        Console.WriteLine("\n" + num[0]);
+        Console.WriteLine("\n" + num[2]);
+      }
+      else {
+        Console.WriteLine("\n" + num[2]);
+        Console.WriteLine("\n" + num[0]);
+      }
+    }
+    else {
+      Console.WriteLine("\n" + num[2]);
+      if (num[0] >= num[1]){
+        Console.WriteLine("\n" + num[0]);
+        Console.WriteLine("\n" + num[1]);
+      }
+      else {
+        Console.WriteLine("\n" + num[1]);
+        Console.WriteLine("\n" + num[0]);
+      }
+    }
+  }
+}
