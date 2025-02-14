18cd0a13e22f1ab9edf43748cb3eb9dc9596f276
ornan-matos <ornanmatos@outlook.com>
2025-02-14 09:16:48 -0300

    feature:inverter valores de variáveis

---
+++ C#/Troca_de_Valores/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class troca{
+
+  static void Main(){
+    Console.Write("\nInsira o valor da variável A: ");
+    int A = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("\nInsira o valor da variável B: ");
+    int B = Convert.ToInt32(Console.ReadLine());
+
+    int C = B;
+
+    B = A;
+
+    A = C;
+
+    Console.WriteLine(String.Format("\nVariável A: {0}\nVariável B: {1}", A, B));
+
+
+  }
+}
