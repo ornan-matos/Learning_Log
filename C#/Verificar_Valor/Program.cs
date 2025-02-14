bde5e0ef6d505270305ebe680da4ff07bf35925a
ornan-matos <ornanmatos@outlook.com>
2025-02-14 11:14:32 -0300

    feature:aplicar estrutura de decisão simples

---
+++ C#/Verificar_Valor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class verificar{
+
+  static void Main(){
+
+    Console.Write("Insira o valor: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+
+    if (num >= 0 && num <= 9){
+
+      Console.WriteLine("\nValor valido!");
+
+    }
+
+    else {
+
+      Console.WriteLine("\nValor invalido!");
+
+    }
+  }
+}
