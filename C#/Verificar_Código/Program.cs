b4dcec22dd9b9af1ab0ffa37e5747f8b375eeca4
ornan-matos <ornanmatos@outlook.com>
2025-02-14 11:23:03 -0300

    feature:verificar número através de estrutura de seleção

---
+++ C#/Verificar_Código/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class codigo{
+
+  static void Main(){
+
+    Console.WriteLine("\nQual o código?");
+    string codigo = Console.ReadLine();
+
+    switch (codigo){
+
+      case "1":
+        Console.WriteLine("\nCódigo 1. \tValido");
+        break;
+
+      case "2":
+        Console.WriteLine("\nCódigo 2. \tValido");
+        break;
+
+      case "3":
+        Console.WriteLine("\nCódigo 3. \tValido");
+        break;
+
+      default:
+        Console.WriteLine("\nCódigo Invalido!");
+        break;
+
+    }
+  }
+}
