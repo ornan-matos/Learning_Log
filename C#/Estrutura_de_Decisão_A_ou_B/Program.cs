767e8d676449d2dcf9997593cae71074cc6cab26
ornan-matos <ornanmatos@outlook.com>
2025-02-17 20:07:43 -0300

    feature:aplicar estrutura de decisão

---
+++ C#/Estrutura_de_Decisão_A_ou_B/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class a_b{
+  static void Main(){
+
+    // Se positivo armazene-o em A, se for negativo, em B. No final mostrar o resultado.
+
+    Console.Write("\nInsira um número: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+
+    int a;
+    int b;
+
+    if (num >= 0){
+      a = num;
+      Console.WriteLine(String.Format("\nArmazenado em A: {0}", a));
+    }
+    else {
+      b = num;
+      Console.WriteLine(String.Format("\nArmazenado em B: {0}", b));
+
+    }
+  }
+}
