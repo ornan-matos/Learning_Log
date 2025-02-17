236eab9fe684b631e49776ff9ef36301d6794fb5
ornan-matos <ornanmatos@outlook.com>
2025-02-17 20:11:12 -0300

    feature:aplicar estrutura de decisão

---
+++ C#/Positivo_Negativo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class pos_neg{
+  static void Main(){
+
+    Console.WriteLine("\nDigite 0 a qualquer momento para sair.");
+      
+    while(true){
+
+      Console.Write("\nInsira o número: ");
+      int num = Convert.ToInt32(Console.ReadLine());
+
+      if (num > 0){
+        Console.WriteLine("\nO número é positivo!");
+      }
+      else if (num == 0){
+        Console.WriteLine("\nPrograma finalizado.");
+        break;
+      }
+      else {
+        Console.WriteLine("\nO número é negativo");
+      }
+
+    }
+
+  }
+}
