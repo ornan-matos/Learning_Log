71afe8e0e5049105b61bdbd278ae4d22956c82d5
ornan-matos <ornanmatos@outlook.com>
2025-02-24 16:19:16 -0300

    feature:aplicar slice, função make e comprimento através do índice do slice

---
+++ GO/Basic/practice/exr_5.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+/*
+Exercício slice, make, comprimento através do índice do slice 
+*/
+
+func main()  {
+
+  x := make([]float32, 5, 10)
+
+  for i := 0; i < len(x); i++{
+  x[i] = float32(i) + 1
+}
+
+fmt.Println(x[2:4])
+  
+}
